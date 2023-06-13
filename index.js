f9bd400031db9c65f43f621d3749c29b20c55cbf
saadrafi <saadrafi81@gmail.com>
2023-06-13 22:30:17 +0600

    api for single class information added

--- index.js
+++ index.js
@@ -84,6 +84,19 @@ async function run() {
       res.send(result);
     });
 
+    // api to get all class information
+    app.get("/class", async (req, res) => {
+      const classes = await classCollection.find({}).toArray();
+      res.send(classes);
+    });
+
+    app.get("/class/:id",async(req,res)=>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await classCollection.findOne(query);
+        res.send(result);
+    }) 
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
