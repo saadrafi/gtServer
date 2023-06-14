293d7de767d6c758462d993dbbe3199b5729e3bc
saadrafi <saadrafi81@gmail.com>
2023-06-14 15:48:04 +0600

    selected class delete api created

--- index.js
+++ index.js
@@ -164,6 +164,14 @@ async function run() {
       }
     });
 
+    // api to delete selected class data
+    app.delete("/selectClass/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectClassCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
