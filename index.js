bf197421001f2ad5c11bb867b0df38ed1cafac64
saadrafi <saadrafi81@gmail.com>
2023-06-16 03:33:07 +0600

    deployed in vercel

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const usersCollection = client.db("globalDB").collection("usersCollection");
     const classCollection = client.db("globalDB").collection("classCollection");
@@ -113,7 +113,7 @@ async function run() {
     });
 
     // api to get all the users
-    app.get("/users", verifyJwt, async (req, res) => {
+    app.get("/users", async (req, res) => {
       const query = req.query.role;
 
       if (query) {
@@ -149,7 +149,7 @@ async function run() {
     });
 
     // api to get all class information
-    app.get("/class", verifyJwt, async (req, res) => {
+    app.get("/class", async (req, res) => {
       const status = req.query.status;
       const query = {};
       if (status) {
@@ -312,8 +312,8 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
